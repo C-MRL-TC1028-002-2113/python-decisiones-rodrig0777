349ff7401188baa4ce4e4ee1123c23603eee3b9e
rodrig0777 <144089002+rodrig0777@users.noreply.github.com>
2023-09-19 10:24:09 -0600

    numeros positivos o negativos

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,8 +1,14 @@
 import math
 def main():
-    #escribe tu código abajo de esta línea
+
     pass
-    
+num= float(input("dame un numero y te dire si es positivo, negativo o cero"))
+if num > 0:
+    print("el numero es positivo")
+elif num < 0:
+    print("el numero es negativo")
+else:
+    print("el numero es cero")
 
 if __name__ == '__main__':
     main()
